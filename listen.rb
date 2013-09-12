b354beb40bd8b0ad360cebcbbd432863c86a092b
Selva <k.n.selvakumar@gmail.com>
2013-09-12 13:55:17 +0530

    Get announcer name with vcard api

--- listen.rb
+++ listen.rb
@@ -7,18 +7,21 @@ Bundler.setup
 require 'blather/client'
 require 'uri'
 
-setup ENV['EMAIL'] || 'thoughtworks.chennai@gmail.com', ENV['PASSWORD']
+ENV['EMAIL'] ||= "thoughtworks.chennai@gmail.com"
+ENV['PLAYER'] ||= "omxplayer"
+ENV['LANGUAGE'] ||= "hi"
+
+setup ENV['EMAIL'], ENV['PASSWORD']
 
 def say(message)
   log(message)
 
-  player = ENV['PLAYER'] || 'omxplayer'
   max_length = 100
   messages = message.scan(/.{1,#{max_length}}\b|.{1,#{max_length}}/).map(&:strip)
 
   messages.each do |m|
-    url = URI.escape("http://translate.google.com/translate_tts?tl=en&q=#{m}")
-    execute %Q{#{player} "#{url}"}
+    url = URI.escape("http://translate.google.com/translate_tts?tl=#{ENV['LANGUAGE']}&q=#{m}")
+    execute %Q{#{ENV['PLAYER']} "#{url}"}
   end
 end
 
@@ -38,13 +41,18 @@ end
 # Say what was said
 message :chat?, :body do |m|
   message = m.body.gsub("\n", " ").gsub("-", "").gsub("/", " or ")
-  message << ".. This message is from: #{m.from.node}"
 
-  say message
+  iq = Blather::Stanza::Iq::Vcard.new :get, "#{m.from.node}@#{m.from.domain}"
+  client.write_with_handler iq do |response|
+    name = ENV["NAME"] || response.vcard["FN"].split(" ")[0]
 
-  reply = m.reply
-  reply.body = "Your wish is my command"
-  write_to_stream reply
+    message << ".. This message is from: #{name}"
+    say message
+
+    reply = m.reply
+    reply.body = "Your wish is my command"
+    write_to_stream reply
+  end
 end
 
 p "Waiting for commands.. Master!!"
